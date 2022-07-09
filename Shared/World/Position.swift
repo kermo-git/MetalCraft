c975354ea0e8ad8dd953b81801a901379350dcd7
Kermo Saarse <saarse27@gmail.com>
2022-07-10 01:55:17 +0300

    Added documentation about coordinate system and updated chunk position calculation.

--- Shared/World/Position.swift
+++ Shared/World/Position.swift
@@ -1,6 +1,98 @@
 import Darwin
 
-struct BlockPos {
+/* * * * * * * * * * * * * * * * * *
+ * BLOCK & POINT COORDINATE SYSTEM *
+ * * * * * * * * * * * * * * * * * *
+ *
+ * TOP DOWN VIEW                                   SIDE VIEW
+ *
+ *           - - - - - -                           - - - - - -
+ *           | North   |                           | Top     |
+ *           |         |                           |         |
+ *           |         |                           |         |
+ *           |         |                           |         |
+ * - - - - - NW- - - - NE- - - - -       - - - - - A - - - - B - - - - -
+ * | West    | Center  | East    |       | North   | Center  | South   |
+ * |         |         |         |       |         |         |         |
+ * |         |         |         |       |         |         |         |
+ * |         |         |         |       |         |         |         |
+ * - - - - - SW- - - - SE- - - - -       - - - - - NW- - - - SW- - - - -
+ *           | South   |                           | Bottom  |
+ *           |         |                           |         |
+ *           |         |                           |         |
+ *           |         |                           |         |
+ *           - - - - - -                           - - - - - -
+ *
+ *  Block coordinates                Point coordinates
+ *
+ *  Center: (X,     Y,     Z    )    NW: (X,     Y,     Z    )
+ *   North: (X,     Y,     Z - 1)    NE: (X + 1, Y,     Z    )
+ *   South: (X,     Y,     Z + 1)    SW: (X,     Y,     Z + 1)
+ *    West: (X - 1, Y,     Z    )    SE: (X + 1, Y,     Z + 1)
+ *    East: (X + 1, Y,     Z    )     A: (X,     Y + 1, Z    )
+ *  Bottom: (X,     Y - 1, Z    )     B: (X,     Y + 1, Z + 1)
+ *     Top: (X,     Y + 1, Z    )
+ *
+ * * * * * * * * * * * * * *
+ * CHUNK COORDINATE SYSTEM *
+ * * * * * * * * * * * * * *
+ *
+ * TOP DOWN VIEW
+ *
+ *           - - - - - -
+ *           | North   |
+ *           |         |
+ *           |         |
+ *           |         |
+ * - - - - - - - - - - - - - - - -
+ * | West    | Center  | East    |
+ * |         |         |         |
+ * |         |         |         |
+ * |         |         |         |
+ * - - - - - - - - - - - - - - - -
+ *           | South   |
+ *           |         |
+ *           |         |
+ *           |         |
+ *           - - - - - -
+ *
+ * Chunk coordinates
+ *
+ * Center: (X,     Z    )
+ * North:  (X,     Z - 1)
+ * South:  (X,     Z + 1)
+ * West:   (X - 1, Z    )
+ * East:   (X + 1, Z    )
+ *
+ * DETAILED VIEW OF A SINGLE CHUNK
+ *
+ *   NW # # # # # # # # # # # # # # NE
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *    # # # # # # # # # # # # # # # #
+ *   SW # # # # # # # # # # # # # # SE
+ *
+ * Corner block local coordinates (Y is omitted)
+ *
+ * NW: (0,  0 )
+ * NE: (15, 0 )
+ * SW: (0,  15)
+ * SE: (15, 15)
+ *
+ * Block with global position of (0, 0) is NW corner of Chunk (0, 0)
+ */
+
+struct BlockPos: Hashable {
     let X: Int
     let Y: Int
     let Z: Int
@@ -43,10 +135,20 @@ struct ChunkPos: Hashable {
     }
 }
 
-func distance(_ chunk1: ChunkPos, _ chunk2: ChunkPos) -> Float {
-    let fX = Float(chunk1.X - chunk2.X)
-    let fZ = Float(chunk1.Z - chunk2.Z)
-    return sqrt(fX * fX + fZ * fZ)
+func getBlockPos(_ pointPos: Float3) -> BlockPos {
+    return BlockPos(X: Int(floor(pointPos.x)),
+                    Y: Int(floor(pointPos.z)),
+                    Z: Int(floor(pointPos.z)))
+}
+
+func getChunkPos(_ pos: BlockPos) -> ChunkPos {
+    func toChunkCoordinate(_ blockCoordinate: Int) -> Int {
+        return (blockCoordinate >= 0) ?
+            blockCoordinate / CHUNK_SIDE :
+            ((blockCoordinate + 1) / CHUNK_SIDE) - 1
+    }
+    return ChunkPos(X: toChunkCoordinate(pos.X),
+                    Z: toChunkCoordinate(pos.Z))
 }
 
 func toGlobalPos(chunk: ChunkPos, local: BlockPos) -> BlockPos {
@@ -55,15 +157,12 @@ func toGlobalPos(chunk: ChunkPos, local: BlockPos) -> BlockPos {
                     Z: chunk.Z * CHUNK_SIDE + local.Z)
 }
 
-func getChunkPos(_ globalPos: Float3) -> ChunkPos {
-    let x = globalPos.x / Float(CHUNK_SIDE)
-    let z = globalPos.z / Float(CHUNK_SIDE)
-    
-    return ChunkPos(X: Int(floor(x)),
-                    Z: Int(floor(z)))
+func getChunkPos(_ pointPos: Float3) -> ChunkPos {
+    return getChunkPos(getBlockPos(pointPos))
 }
 
-func getChunkPos(_ globalPos: BlockPos) -> ChunkPos {
-    ChunkPos(X: globalPos.X / CHUNK_SIDE,
-             Z: globalPos.Z / CHUNK_SIDE)
+func distance(_ chunk1: ChunkPos, _ chunk2: ChunkPos) -> Float {
+    let fX = Float(chunk1.X - chunk2.X)
+    let fZ = Float(chunk1.Z - chunk2.Z)
+    return sqrt(fX * fX + fZ * fZ)
 }
