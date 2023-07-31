c5293c2aec411baabfa9061221df6af0501f1c16
Kermo Saarse <saarse27@gmail.com>
2023-07-31 19:14:01 +0300

    Drag gesture to move camera on iOS

--- iOS/App.swift
+++ iOS/App.swift
@@ -2,12 +2,20 @@ import SwiftUI
 
 let camera = FlyingCamera(startPos: Float3(0, 60, 0))
 let renderer = WorldRenderer(generator: generateChunk, camera: camera)
+let DRAG_SENSITIVITY: Float = 0.05
 
 @main
 struct MetalCraftApp: App {
     var body: some Scene {
         WindowGroup {
-            MetalView(renderer: renderer)
+            MetalView(renderer: renderer).gesture(
+                DragGesture()
+                    .onChanged { value in
+                        let width = Float(value.translation.width)
+                        let height = Float(value.translation.height)
+                        Mouse.move(width * DRAG_SENSITIVITY, height * DRAG_SENSITIVITY)
+                    }
+            )
         }
     }
 }
